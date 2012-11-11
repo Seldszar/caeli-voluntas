571b483cec5007c9276889bf95098bfca39566aa
Seldszar <0xfuture@gmail.com>
2012-11-11 11:39:54 +0100

    Correction du calcul du champ heroic_mode qui n'est vrai que lorsque qu'au moins un boss en mode héroïque est mort

--- Model/EncounterZone.php
+++ Model/EncounterZone.php
@@ -18,7 +18,7 @@ class EncounterZone extends AppModel {
 	var $virtualFields = array(
 		'normal_progress_percentage' => 'FLOOR(normal_progress / num_bosses * 100)',
 		'heroic_progress_percentage' => 'FLOOR(heroic_progress / num_bosses * 100)',
-		'heroic_mode' => 'normal_progress = num_bosses'
+		'heroic_mode' => 'heroic_progress > 0'
 	);
 
 }
