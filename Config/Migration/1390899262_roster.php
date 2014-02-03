aec39bb4bb12b23af68e099a9ed92a062201eca4
Seldszar <0xfuture@gmail.com>
2014-02-03 09:58:35 +0100

    Renommage du champ visible en visible_in_roster

---
+++ Config/Migration/1390899262_roster.php
@@ -0,0 +1,56 @@
+<?php
+class Roster extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+            'rename_field' => array(
+                'groups' => array(
+                    'visible' => 'visible_in_roster'
+                )
+            )
+		),
+		'down' => array(
+            'rename_field' => array(
+                'groups' => array(
+                    'visible_in_roster' => 'visible'
+                )
+            )
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
