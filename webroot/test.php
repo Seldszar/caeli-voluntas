a5360f33fa7a11a46d403d7f21706dfaaab09994
Seldszar <0xfuture@gmail.com>
2014-02-27 11:01:54 +0100

    Intégration de Composer

--- webroot/test.php
+++ webroot/test.php
@@ -53,7 +53,7 @@ if (!defined('APP_DIR')) {
  *
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
-	//define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . DS . 'Vendor' . DS . 'pear-pear.cakephp.org' . DS . 'CakePHP');
 
 /**
  * Editing below this line should not be necessary.
