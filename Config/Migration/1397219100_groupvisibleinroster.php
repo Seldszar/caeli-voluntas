0ba26cb793d0e84db7e4f78d59f624de8bc50d1d
Seldszar <0xfuture@gmail.com>
2014-04-11 14:32:23 +0200

    Ajout de la migration ajoutant la colonne 'visible_in_roster'. Suppression d'une migration qui est éronnée.

---
+++ Config/Migration/1397219100_groupvisibleinroster.php
@@ -0,0 +1,54 @@
+<?php
+class GroupVisibleInRoster extends CakeMigration {
+
+	/**
+	 * Migration description
+	 *
+	 * @var string
+	 * @access public
+	 */
+	public $description = 'Gestion de la visibilité du groupe dans le roster';
+
+	/**
+	 * Actions to be performed
+	 *
+	 * @var array $migration
+	 * @access public
+	 */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'groups' => array(
+					'visible_in_roster' => array('type' => 'boolean', 'null' => false, 'default' => true)
+				)
+			)
+		),
+		'down' => array(
+			'drop_field' => array(
+				'groups' => array('visible_in_roster')
+			)
+		),
+	);
+
+	/**
+	 * Before migration callback
+	 *
+	 * @param string $direction, up or down direction of migration process
+	 * @return boolean Should process continue
+	 * @access public
+	 */
+	public function before($direction) {
+		return true;
+	}
+
+	/**
+	 * After migration callback
+	 *
+	 * @param string $direction, up or down direction of migration process
+	 * @return boolean Should process continue
+	 * @access public
+	 */
+	public function after($direction) {
+		return true;
+	}
+}
